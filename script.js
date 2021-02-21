d636b4f45abfdc55005d1a5257a8f3bfbf1b0af9
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-02-21 21:47:44 +0100

    Search and Recipe numbers and validations added

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 
 const YOUR_APP_ID = "f03a225a"
 const YOUR_APP_KEY = "6e484f358af33d3806801aa4a000c22f" 
-const mainIngredient = document.getElementById('main-ingredient').value
-const numberOfRecipe = 10
-const imageSize ="THUMBNAIL"
-const EDAMAM_URL=`https://api.edamam.com/search?q=${mainIngredient}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=${numberOfRecipe}&calories=591-722&health=alcohol-free&imageSize=${imageSize}`
 
 
-const buildRecipeList = (data) => {
+const buildRecipeList = (json) => {
   const recipeList = document.getElementById('recipeList')
   let output = ""
-  let ingredients
-  data.hits.forEach((recipeContainer) => {
+  json.hits.forEach((recipeContainer) => {
   console.log(recipeContainer)
-
   output += `
             <section class="recipe-card">
                 <p class="card-name">
@@ -37,11 +31,17 @@ const buildRecipeList = (data) => {
   })
 }
 
-const fetchEdamamData = () => {
+const fetchEdamamData = (event) => {
+  event.preventDefault()
+  const mainIngredient = document.getElementById('main-ingredient').value
+  const numberOfRecipe = document.getElementById('number-of-recipes').value
+  const imageSize ="THUMBNAIL"
+  const EDAMAM_URL=`https://api.edamam.com/search?q=${mainIngredient}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=${numberOfRecipe}&calories=591-722&health=alcohol-free&imageSize=${imageSize}`
+
   fetch(EDAMAM_URL)
     .then((response) => {return response.json()})
-    .then((data) => {
-      buildRecipeList(data)
+    .then((json) => {
+      buildRecipeList(json)
     }).catch((error) => {
       console.log(error, "There has been an error")
     })
