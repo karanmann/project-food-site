41b1d54779ef21579b930bacd27fd23ce831b9d2
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-02-21 15:29:48 +0100

    Styling Added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const YOUR_APP_ID = "f03a225a"
 const YOUR_APP_KEY = "6e484f358af33d3806801aa4a000c22f" 
-const mainIngredient = "vegan"
+const mainIngredient = document.getElementById('main-ingredient').value
 const numberOfRecipe = 10
 const imageSize ="THUMBNAIL"
 const EDAMAM_URL=`https://api.edamam.com/search?q=${mainIngredient}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=${numberOfRecipe}&calories=591-722&health=alcohol-free&imageSize=${imageSize}`
@@ -16,13 +16,21 @@ const buildRecipeList = (data) => {
 
   output += `
             <section class="recipe-card">
-              <p class="card-name">${recipeContainer.recipe.label}</p>
-              <img class="card-image" src="${recipeContainer.recipe.image}"></img>
-              <p>Yield : ${recipeContainer.recipe.yield}</p>
-              <p>Healthiness - ${recipeContainer.recipe.healthLabels}</p>
-              <p> Source - ${recipeContainer.recipe.source}</p>
-              <p> Recipe </p>
-
+                <p class="card-name">
+                  <b>🍲 ${recipeContainer.recipe.label}</b>
+                </p>
+                <a href="${recipeContainer.recipe.url}">
+                  <img class="card-image" src="${recipeContainer.recipe.image}"></img>
+                </a>
+                <p class="card-yield"><b>Yield -</b> ${recipeContainer.recipe.yield}</p>
+                <p class="card-healthiness"><b>Healthiness -</b> ${recipeContainer.recipe.healthLabels}</p>
+                <a href="${recipeContainer.recipe.url}">
+                  <button class="card-button">Recipe</button>
+                </a>
+                <p class="source">
+                  <b><i>Source - ${recipeContainer.recipe.source}</i></b>
+                </p>
+              
             </section>
             `
   recipeList.innerHTML = output
